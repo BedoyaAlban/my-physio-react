a2be5a5003a7bf6329d62c39fd06b169930b2277
Bamboo <a.bedoya@outlook.fr>
2019-08-31 16:17:54 +0200

    essai

---
+++ assets/js/images.js
@@ -0,0 +1,3 @@
+require('./images/agenda.png');
+require('./images/clients.png');
+require('./images/factures.png');
--- assets/js/pages/MyPhysio.jsx
+++ assets/js/pages/MyPhysio.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-
+const agenda = require('../images/agenda.png');
+const clients = require('../images/clients.png');
+const factures = require('../images/factures.png');
 //Composant MyPhysio
 const MyPhysio = (props) => {
 
@@ -12,19 +14,19 @@ const MyPhysio = (props) => {
                 <div className="col-lg-4">
                     <div className="card mb-5 mb-lg-0">
                         <h3>Agenda</h3>
-                        <img className="img-fluid img-thumbnail" src="" alt="agenda"/>
+                        <img className="img-fluid img-thumbnail" src={agenda} alt="agenda"/>
                     </div>
                 </div>
                 <div className="col-lg-4">
                     <div className="card mb-5 mb-lg-0">
                         <h3>Clients</h3>
-                        <img className="img-fluid img-thumbnail" src="" alt="liste des clients"/>
+                        <img className="img-fluid img-thumbnail" src={clients} alt="liste des clients"/>
                     </div>
                 </div>
                 <div className="col-lg-4">
                     <div className="card mb-5 mb-lg-0">
                         <h3>Factures</h3>
-                        <img className="img-fluid img-thumbnail" src="" alt="liste des factures"/>
+                        <img className="img-fluid img-thumbnail" src={factures} alt="liste des factures"/>
                     </div>
                 </div>
                 <div className="col-md-8 mx-auto">
