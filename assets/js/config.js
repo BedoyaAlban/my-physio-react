1114c15ee7055786167ce442af15b088c3de4c79
Bamboo <a.bedoya@outlook.fr>
2019-08-31 16:32:48 +0200

    essai

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,3 +5,8 @@ export const DIARY_API = API_URL +  "diaries";
 export const INVOICES_API = API_URL +  "invoices";
 export const USERS_API = API_URL +  "users";
 export const LOGIN_API = API_URL +  "login_check";
+
+
+
+
+
